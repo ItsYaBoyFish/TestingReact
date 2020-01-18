5a7a9683c2cc117dec83736216add32e6936e741
Austin Graves <austingraves106@aol.com>
2020-01-17 19:30:15 -0600

    Gonna attempt to deploy just the ui to netlify

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import Logo from './components/landingPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LoginPage from './pages/LoginPage/loginPage';
+import ViewEvents from './pages/ViewEvents/viewEvents';
+import AdminPage from './pages/AdminPage/AdminPage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Logo />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={LoginPage} />
+          <Route exact path="/dashboard" component={AdminPage} />
+          <Route exact path="/event/:id" component={ViewEvents} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
