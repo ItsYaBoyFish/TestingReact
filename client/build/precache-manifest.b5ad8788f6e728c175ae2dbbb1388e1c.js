eed5e8efbafb5fbcef69b5acb4e6e1b33fab5d0d
Austin Graves <austingraves106@aol.com>
2020-01-17 19:51:29 -0600

    removed the git ignore for build

---
+++ client/build/precache-manifest.b5ad8788f6e728c175ae2dbbb1388e1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecf476d0e846091f3ed2797368e5f71d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fc5b76bee0996fa8409",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "494e70970a051d25fd6a",
+    "url": "/static/css/main.99f15dd6.chunk.css"
+  },
+  {
+    "revision": "2fc5b76bee0996fa8409",
+    "url": "/static/js/2.0d567c5d.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.0d567c5d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "494e70970a051d25fd6a",
+    "url": "/static/js/main.077a95a9.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
