428edf829f58820d4ad1d0a5ef2196c090e4f587
Austin Graves <austingraves106@aol.com>
2020-01-17 19:15:18 -0600

    Added the use static files option in server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+const path = require('path');
 
 app.get('/customers', (req, res) => {
   var customers = [
@@ -14,6 +15,8 @@ app.get('/customers', (req, res) => {
     Customers: customers
   })
 });
+
+app.use(express.static(path.join(__dirname, 'client')));
 
 
 app.listen(port, () => {
